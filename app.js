7aff4c848f7660d69ebefb3d89ae67c69f127c6c
Khallo <khaalid991@gmail.com>
2024-11-26 14:02:50 +0300

    done

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// let userName = window.prompt("What is your username")
+const increaseBtn = document.getElementById("increaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const decreaseBtn = document.getElementById("decreaseBtn")
+const countLabel = document.getElementById("countLabel")
+let count = 0
 
-// console.log(userName)
-
-let userName
-
-document.getElementById("submit").onclick = function(){
-    userName = document.getElementById("mytext").value
-    document.getElementById("myH1").textContent = `Hello ${userName}`
+increaseBtn.onclick = function(){
+    count ++
+    countLabel.textContent = count
+}
+decreaseBtn.onclick = function(){
+    count --
+    countLabel.textContent = count
+}
+resetBtn.onclick = function(){
+    count = 0 
+    countLabel.textContent = count
 }
